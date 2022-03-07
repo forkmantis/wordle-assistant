f18902f330bf930d8fec00a60aa23e2f2e628e96
Will Killian <github.user@forkmantis.com>
2022-03-06 19:54:15 -0600

    Add initial loop for a wordle solving session

--- assistant.rb
+++ assistant.rb
@@ -3,3 +3,13 @@ DICTIONARY_FILE = "./resources/dictionary.txt"
 words = File.readlines(DICTIONARY_FILE)
 
 words.each {|word| puts word }
+
+solved = false
+
+while !solved
+  puts "Have you solved the puzzle? (y/n)"
+  solved = gets.chomp[0].match(/[Yy]/)
+  puts "OK. Try again"
+end
+
+puts "Congratulations!"
